5c560a9d20d8fc28c479dd6907b50e6b38b669ec
Damien Ollerhead <sxywytboy@gmail.com>
2025-05-12 09:58:40 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,27 @@
 // pages/_app.js
 import '../styles/globals.css';
+import Image from 'next/image'; // Import Image for Next.js image optimization
+import Link from 'next/link';   // Import Link for navigation
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      {/* Header Section */}
+      <header className="header">
+        <Link href="/">
+          <Image
+            src="/poolpass-logo.png" // Path to your logo image
+            alt="PoolPass Logo"
+            width={120} // Adjust width as necessary
+            height={40} // Adjust height as necessary
+          />
+        </Link>
+      </header>
+
+      {/* Main Content of the Page */}
+      <main className="container">
+        <Component {...pageProps} />
+      </main>
+    </>
+  );
 }
