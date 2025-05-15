27e8ec4292716f178b0bceb845caced2d221056c
Damien Ollerhead <sxywytboy@gmail.com>
2025-05-15 15:13:56 +0100

    Update thankyou.js

--- pages/thankyou.js
+++ pages/thankyou.js
@@ -1,13 +1,28 @@
-// pages/thankyou.js
 import Link from 'next/link';
 
 export default function ThankYou() {
   return (
-    <div style={{ padding: 20, textAlign: 'center' }}>
+    <div style={{ padding: '40px', textAlign: 'center' }}>
       <h1>Thank You!</h1>
       <p>Your pool has been submitted and will appear on Pool Pass soon.</p>
       <Link href="/">
-        <a>Back to Home</a>
+        <a>
+          <button
+            type="button"
+            style={{
+              marginTop: '20px',
+              padding: '12px 24px',
+              fontSize: '16px',
+              cursor: 'pointer',
+              borderRadius: '6px',
+              border: 'none',
+              backgroundColor: '#0070f3',
+              color: 'white',
+            }}
+          >
+            Back to Home
+          </button>
+        </a>
       </Link>
     </div>
   );
