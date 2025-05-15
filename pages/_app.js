7b27efd73896742f884395e273d7e8e7c986314e
Damien Ollerhead <sxywytboy@gmail.com>
2025-05-15 14:18:55 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -6,47 +6,35 @@ import Link from 'next/link';
 export default function App({ Component, pageProps }) {
   return (
     <>
-      {/* Header Section */}
-      <header className="header">
-        <div className="header-logo-wrapper">
-          <Link href="/">
-            <a aria-label="PoolPass Home">
-              <Image
-                src="/poolpass_logo.jpeg" // Make sure the filename is lowercase and no spaces
-                alt="PoolPass Logo"
-                width={300}  // Bigger logo width
-                height={100} // Adjust height for aspect ratio
-                priority={true}
-              />
-            </a>
-          </Link>
-        </div>
+      <header>
+        <Link href="/">
+          <a>
+            <Image
+              src="/poolpass_logo.jpeg" // Make sure file name matches exactly (case sensitive)
+              alt="PoolPass Logo"
+              width={auto}
+              height={160} // Bigger logo height to match CSS
+              className="header-logo"
+              style={{ width: 'auto', height: '160px' }}
+              priority
+            />
+          </a>
+        </Link>
 
-        <div className="header-banner-wrapper">
-          <Image
-            src="/banner.jpg"
-            alt="PoolPass Banner"
-            width={1200} // Banner width
-            height={200} // Thicker banner height
-            priority={true}
-            objectFit="cover"
-          />
-        </div>
+        <Image
+          src="/banner.jpg" // Banner image file
+          alt="Banner"
+          width={1200} // Banner width, adjust as needed
+          height={300} // Banner height matches CSS max-height
+          className="header-banner"
+          priority
+        />
 
         <nav className="nav">
-          <Link href="/">
-            <a>Home</a>
-          </Link>
-          <Link href="/pools">
-            <a>Pools</a>
-          </Link>
-          <Link href="/contact">
-            <a>Contact</a>
-          </Link>
+          {/* Add your nav links here */}
         </nav>
       </header>
 
-      {/* Main Content */}
       <main className="container">
         <Component {...pageProps} />
       </main>
