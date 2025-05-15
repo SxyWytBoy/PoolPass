86bef689624ccf7c1e036adad7975799ccdce58f
Damien Ollerhead <sxywytboy@gmail.com>
2025-05-15 15:12:40 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
+  // No output: 'export' here because you want server-side rendering on Render
 };
+
+module.exports = nextConfig;
